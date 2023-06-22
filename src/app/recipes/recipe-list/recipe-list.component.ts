d9f56839ddde1e44cdf9f0963109f916576ff75b
drocpdp <davidreynon@gmail.com>
2023-06-22 09:48:53 +0200

    118

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,21 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe1','1This is simply a test','https://www.cookincanuck.com/wp-content/uploads/2022/03/Caramelized-Cabbage-Recipe-Square-1200-1.jpg'),
-    new Recipe('A Test Recipe2','2This is simply a test','https://clovegarden.com/ingred/img/ig_kimchee01f.jpg'),
-    new Recipe('A Test Recipe3','3This is simply a test','https://www.cookincanuck.com/wp-content/uploads/2022/03/Caramelized-Cabbage-Recipe-Square-1200-1.jpg'),
- ];
+export class RecipeListComponent implements OnInit {
+  @Output() triggerRecipeSelectionRelay = new EventEmitter<Recipe>;
+  recipes: Recipe[];
 
- @Output() triggerRecipeSelectionRelay = new EventEmitter<Recipe>;
+  constructor(private recipeService: RecipeService){}
 
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   itemTriggered(ev: Recipe){
     console.log("itemTriggered() in recipe-list component");
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService{
-    
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe1','1This is simply a test','https://www.cookincanuck.com/wp-content/uploads/2022/03/Caramelized-Cabbage-Recipe-Square-1200-1.jpg'),
+        new Recipe('A Test Recipe2','2This is simply a test','https://clovegarden.com/ingred/img/ig_kimchee01f.jpg'),
+        new Recipe('A Test Recipe3','3This is simply a test','https://www.cookincanuck.com/wp-content/uploads/2022/03/Caramelized-Cabbage-Recipe-Square-1200-1.jpg'),
+     ];
+
+     getRecipes() {
+        return this.recipes.slice();
+     }
+
 }
